8aa7e1d7ffe5eecd14c74e5c136c9e56e11c6e49
Santiago Corbanini <santiagocorbanini@gmail.com>
2025-06-09 19:32:41 -0300

    cambio datos de firebase

--- front/src/firebaseConfig/firebase.js
+++ front/src/firebaseConfig/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "@firebase/firestore"
-
+/*
 const firebaseConfig = {
     apiKey: "AIzaSyDSHj8Ka_m7QGlWNSXitHoroTnz_wKnN2Y",
     authDomain: "crud-2222agencia.firebaseapp.com",
@@ -8,6 +8,16 @@ const firebaseConfig = {
     storageBucket: "crud-2222agencia.appspot.com",
     messagingSenderId: "464704355531",
     appId: "1:464704355531:web:0a5ca615c3ff2c4ec6af4e",
+}
+*/
+const firebaseConfig = {
+    apiKey: "AIzaSyCAYtI1P7cnF8uffI0kwPtWWeaoi7l_hQM",
+    authDomain: "unplanenjunin-70d78.firebaseapp.com",
+    projectId: "unplanenjunin-70d78",
+    storageBucket: "unplanenjunin-70d78.firebasestorage.app",
+    messagingSenderId: "300192793297",
+    appId: "1:300192793297:web:61499aee7a08c744797bf0",
+    measurementId: "G-TGJ8C9BC6R"
 }
 
 export const app = initializeApp(firebaseConfig)
